5a8d3b9449579178fda1e8e05be6b94a11049cf0
m_mitin <maximus405@gmail.com>
2022-08-01 01:36:41 +0300

    adding Hero structure for Heroes VC

---
+++ hmm3_w_storyboard/Model/Heroes.swift
@@ -0,0 +1,19 @@
+//
+//  Heroes.swift
+//  hmm3_w_storyboard
+//
+//  Created by Maxim Mitin on 1.08.22.
+//
+
+import Foundation
+
+struct Hero {
+    let name: String
+    let heroImg: String
+    let description: String
+    let heroClass: String
+    let heroSpec: String
+    let firstSkill: String
+    let secondSkill: String?
+    let heroSpell: String?
+}

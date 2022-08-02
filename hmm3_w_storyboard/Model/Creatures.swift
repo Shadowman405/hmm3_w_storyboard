16258aaf43ff795a836234735433ae4a6b6359bd
m_mitin <maximus405@gmail.com>
2022-08-02 22:46:25 +0300

    add some pics of creatures
    create array with few castle creatures

---
+++ hmm3_w_storyboard/Model/Creatures.swift
@@ -0,0 +1,20 @@
+//
+//  Creatures.swift
+//  hmm3_w_storyboard
+//
+//  Created by Maxim Mitin on 2.08.22.
+//
+
+import Foundation
+
+struct Creature{
+    let name: String
+    let creatureImg: String
+    let attack: String
+    let defense: String
+    let damage: String
+    let health: String
+    let speed: String
+    let specAbility: String?
+    let race: raceTown
+}

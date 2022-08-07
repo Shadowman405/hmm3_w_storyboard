7daa05a9686cb9b73ab5fbad198fb9a7f1ab6a3b
m_mitin <maximus405@gmail.com>
2022-08-07 13:09:11 +0300

    configure segue and data pass from Magic schools to spells VC

--- hmm3_w_storyboard/Controller/SpellsCategoryTVC.swift
+++ hmm3_w_storyboard/Controller/SpellsCategoryTVC.swift
@@ -25,6 +25,10 @@ class SpellsCategoryTVC: UITableViewController {
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 150
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "toSelectedMagic", sender: self)
+    }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -33,6 +37,16 @@ class SpellsCategoryTVC: UITableViewController {
                 return cell
         }
         return UITableViewCell()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toSelectedMagic",
+           let destination = segue.destination as? SpellsTVC{
+            let indexPath = tableView.indexPathForSelectedRow!
+            if indexPath.row == 0 {
+                destination.selectedSpells = data.spells.filter {$0.spellSchool == .AllSchools  }
+            }
+        }
     }
 
 }
